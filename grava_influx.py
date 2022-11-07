693dea60645ebb7fa448a0027b434dcf4eda0157
Daniel Noronha <54832369+danielns-op@users.noreply.github.com>
2022-11-07 10:01:07 -0300

    Create grava_influx.py

---
+++ grava_influx.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# - grava_influx.py ------------------------------------------------- #
+# ------------------------------------------------------------------- #
+# Author: Daniel Noronha da Silva C012743                             #
+#  Phone: +55 85 987680611                                            #
+# ------------------------------------------------------------------- #
+# - Descrição ------------------------------------------------------- #
+#   Script para gravação de dados no influxDB.                        #
+# ------------------------------------------------------------------- #
+
+# - Imports --------------------------------------------------------- #
+from datetime import datetime
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
+# ------------------------------------------------------------------- #
+
+# - Classes --------------------------------------------------------- #
+class GravaInfluxDB:
+  def __init__(self, url, token, org, bucket, measurement):
+    self.url = url
+    self.token = token
+    self.org = org
+    self.bucket = bucket
+    self.measurement = measurement
+
+  def grava_influx(self, tag_dict, field_dict, time):
+    client = InfluxDBClient(
+      url=self.url,
+      token=self.token,
+      org=self.org)
+
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    write_api.write(self.bucket, self.org, {"measurement": self.measurement, "tags": tag_dict, "fields": field_dict, "time": time})
+    client.close()
+
+
+# ------------------------------------------------------------------- #
